748b437f723e14ce0ac1c9a6bb96dcb81f58283b
Stefan Medves <stefan.medves@gmail.com>
2021-06-18 21:55:38 +0100

    feat: Add class and method to return a promise when selecting a category

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+let buttons = document.getElementsByTagName('button');
+
+class ApiRequest {
+  sendHTTPRequest (type, url) {
+    var promise = new Promise(function(resolve, reject) {
+      const request = new XMLHttpRequest();
+      request.onload = function() {
+        if(request.readyState == 4) {
+          if(request.status == 200) {
+            let asJSON = JSON.parse(request.responseText);
+            resolve(asJSON);
+          } else {
+            reject(console.log("Oops! Something went wrong"));
+          }
+        }
+      }
+      request.open(type, url);
+      request.send();
+    });
+    return promise;
+  }
+}
+
+for(let button of buttons) {
+  button.addEventListener('click', function(e) {
+    let id = e.target.id;
+    let client = new ApiRequest();
+    let questionCategory = id === 'mixed' ?
+                                  client.sendHTTPRequest('GET', 'https://opentdb.com/api.php?amount=10') :
+                                  client.sendHTTPRequest('GET', 'GET', `https://opentdb.com/api.php?amount=10&category=${id}`)
+    questionCategory.then((data) => {
+      console.log(data.results)
+    })
+    console.log(id)
+  })
+}
