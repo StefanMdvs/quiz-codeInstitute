5a9bcaa3148b45e215e590645903573bf80d419c
Stefan Medves <stefan.medves@gmail.com>
2021-07-01 12:49:55 +0100

    fix: Display different text message depending on the final right questions

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,7 +1,10 @@
 let final = document.getElementById('end');
 
 const rightQuestions = localStorage.getItem('rightQuestions');
-
-final.innerText = rightQuestions == 5 ? 
-"You're a star! You have answered correctly to ALL questions!" :
-`Well done! You have replied correctly to ${rightQuestions} questions! Keep practicing!`;
+if(rightQuestions == 0){
+  final.innerText = "Oh, no! You didn't get any questions right. Don't give up, keep practicing!";
+} else if(rightQuestions == 10) {
+  final.innerText = "You're a star! You have answered correctly to ALL questions!";
+} else {
+  final.innerText = `Well done! You have replied correctly to ${rightQuestions} questions! Keep practicing!`;
+}
