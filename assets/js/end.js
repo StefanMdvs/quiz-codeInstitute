96d85c44eb8dc97870f5bf4fb53585b1e6143b88
Stefan Medves <stefan.medves@gmail.com>
2021-07-04 14:50:39 +0100

    fix: Fix typo in final message displayed

--- assets/js/end.js
+++ assets/js/end.js
@@ -2,9 +2,9 @@ let final = document.getElementById('end');
 
 const rightQuestions = localStorage.getItem('rightQuestions');
 if(rightQuestions == 0){
-  final.innerText = "Oh, no! You didn't get any questions right. Don't give up, keep practicing!";
+  final.innerText = "Oh, no! You didn't get any answers right. Don't give up, keep practicing!";
 } else if(rightQuestions == 10) {
   final.innerText = "You're a star! You have answered correctly to ALL questions!";
 } else {
-  final.innerText = `Well done! You have replied correctly to ${rightQuestions} questions! Keep practicing!`;
+  final.innerText = `Well done! You have answered correctly to ${rightQuestions} questions! Keep practicing!`;
 }
