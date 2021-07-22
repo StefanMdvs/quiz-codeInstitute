2a59aeffc80a5ef37dc0de7f1b5f02bbfa547383
Stefan Medves <stefan.medves@gmail.com>
2021-07-22 19:20:29 +0100

    fix: Add strict equality operator to fix type error

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,13 +1,9 @@
-function displayResult () {
-  let final = document.getElementById('end');
-
-  const rightQuestions = localStorage.getItem('rightQuestions');
-  if(rightQuestions == 0){
-    final.innerText = "Oh, no! You didn't get any answers right. Don't give up, keep practicing!";
-  } else if(rightQuestions == 10) {
-    final.innerText = "You're a star! You have answered correctly to ALL questions!";
-  } else {
-    final.innerText = `Well done! You have answered correctly to ${rightQuestions} questions! Keep practicing!`;
-  }
+let final = document.getElementById('end');
+const rightQuestions = localStorage.getItem('rightQuestions');
+if(rightQuestions === '0'){
+  final.innerText = "Oh, no! You didn't get any answers right. Don't give up, keep practicing!";
+} else if(rightQuestions === '10') {
+  final.innerText = "You're a star! You have answered correctly to ALL questions!";
+} else {
+  final.innerText = `Well done! You have answered correctly to ${rightQuestions} questions! Keep practicing!`;
 }
-document.addEventListener('load', displayResult);
