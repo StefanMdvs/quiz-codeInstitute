de620abde4cfecb0426fb4d834d061bad5b98147
Stefan Medves <stefan.medves@gmail.com>
2021-07-22 12:43:46 +0100

    fix: Fix console type error by adding event listener

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,10 +1,13 @@
-let final = document.getElementById('end');
+function displayResult () {
+  let final = document.getElementById('end');
 
-const rightQuestions = localStorage.getItem('rightQuestions');
-if(rightQuestions == 0){
-  final.innerText = "Oh, no! You didn't get any answers right. Don't give up, keep practicing!";
-} else if(rightQuestions == 10) {
-  final.innerText = "You're a star! You have answered correctly to ALL questions!";
-} else {
-  final.innerText = `Well done! You have answered correctly to ${rightQuestions} questions! Keep practicing!`;
+  const rightQuestions = localStorage.getItem('rightQuestions');
+  if(rightQuestions == 0){
+    final.innerText = "Oh, no! You didn't get any answers right. Don't give up, keep practicing!";
+  } else if(rightQuestions == 10) {
+    final.innerText = "You're a star! You have answered correctly to ALL questions!";
+  } else {
+    final.innerText = `Well done! You have answered correctly to ${rightQuestions} questions! Keep practicing!`;
+  }
 }
+document.addEventListener('load', displayResult);
